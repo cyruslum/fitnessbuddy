8ef4066e85b90cc0b68ffa828d8664a8f1dca962
cyruslum <cyruslum@hotmail.com>
2026-08-07 14:20:33 -0700

    Update stripe_config.php

--- Fitness-Buddy-master/stripe_config.php
+++ Fitness-Buddy-master/stripe_config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'secret_key'        => 'sk_test_51QrrAOC00vz9v8F7UJXPHKNS6JiTuYyZZjMUQxuhYGC4Jmk2qKIeIn1Hce70tGVdaS3yPNfIhR85F3EDrQ0wmhnF00NeXmMugd',
-    'webhook_secret'     => 'whsec_57f42982b23cf4d6197d0ea7fa2b1498d5093bfb184bf7db751738e3162265d9',
+    'secret_key'        => 'INSERT_SECRET_KEY',
+    'webhook_secret'     => 'INSERT_WEBHOOK',
     'monthly_price_id'   => 'price_1U0y8DC00vz9v8F75e8xw4Jf',
     'annual_price_id'    => 'price_1U0y97C00vz9v8F7dmNfQWy9',
     'app_url'            => 'http://localhost/fitness_buddy',
